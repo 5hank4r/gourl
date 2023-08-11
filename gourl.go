7a1f13ae6c15cc67382169d6169bf21f1c1079b2
shankar acharya <34772838+5hank4r@users.noreply.github.com>
2023-08-11 21:49:53 +0545

    Update gourl.go

--- gourl.go
+++ gourl.go
@@ -9,17 +9,18 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: gourl <custom_keyword>")
+		fmt.Println("Usage: urlexgo <custom_keyword>")
 		os.Exit(1)
 	}
 
-	customPattern := os.Args[1]
-	pattern := fmt.Sprintf(`(https?://)?(www\.)?%s[^ /]+`, regexp.QuoteMeta(customPattern))
+	customKeyword := os.Args[1]
+	pattern := fmt.Sprintf(`(https?://)?(www\.)?([a-zA-Z0-9.-]+\.)?%s\.[a-z]+(/\S*)?`, regexp.QuoteMeta(customKeyword))
+	compiledPattern := regexp.MustCompile(pattern)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
-		if matched, _ := regexp.MatchString(pattern, line); matched {
+		if compiledPattern.MatchString(line) {
 			fmt.Println(line)
 		}
 	}
